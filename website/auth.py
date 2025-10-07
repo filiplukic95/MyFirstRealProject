f89f0b72d3b1045909ef005486c25c3c32dae222
Filip Lukic <filiplukic95@gmail.com>
2025-10-07 18:09:15 +0200

    create login user

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,29 @@
-from flask import Blueprint, render_template,request
+from flask import Blueprint, render_template,request,redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
+from flask_login import login_user,login_required,logout_user,current_user
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login",methods=["GET", "POST"])
 def login():
-    data=request.form
-    print(data)
+    if request.method=="POST":
+        email=request.form.get("email")
+        password=request.form.get("password")
+
+        user=User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password_hash,password):
+                login_user(user,remember=True)
+                return redirect(url_for("views.survey"))
     return render_template("login.html")
 
 @auth.route("/logout")
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("auth.login"))
 
 @auth.route("/sign-up",methods=["GET", "POST"])
 def sign_up():
@@ -18,7 +31,19 @@ def sign_up():
         fullname=request.form.get("full-name")
         email=request.form.get("email")
         password=request.form.get("password")
-        confirm_password=request.form.get("confirm-password")
+        #confirm_password=request.form.get("confirm-password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            pass
+        else:
+            new_user=User(email=email,full_name=fullname,password_hash=generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+
+            return redirect(url_for("views.survey"))
+
     return render_template("register.html")
 
 
